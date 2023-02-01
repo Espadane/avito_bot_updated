38f9c3f61265e8e2e755af9aa9e217de58ebcfae
espadane <espadane@gmail.com>
2023-02-01 15:14:02 +0300

    update

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def text_gandler(msg: types.Message):
         обработка сообщений пользователя
     """
     user_id = int(msg.from_id)
-    if user_id != ADMIN_ID:
+    if user_id !=int( ADMIN_ID):
         await msg.answer('Ты не авторизованный пользователь. Если хочешь\
  пользаться ботом напиши @espadane.')
     else:
